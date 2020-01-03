d9f19c7247a4cc077dac2b08302c263da35f2cce
Teppo Lainio <teppo.lainio@gmail.com>
2020-01-03 13:15:42 +0200

    Initial version of regular expression rule.

---
+++ CdsCustomizationValidator/CdsCustomizationValidator.Domain/Rule/RegexRule.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CdsCustomizationValidator.Domain.Rule
+{
+
+
+    public enum RuleScope { 
+        Entity
+    };
+
+    /// <summary>
+    /// Rule which allows to define rules using regular expression.
+    /// Expressions need to be in .NET regular expression format.
+    /// </summary>
+    public class RegexRule : CustomizationRuleBase
+    {
+
+        /// <summary>
+        /// See <see cref="CustomizationRuleBase.Description"/>.
+        /// </summary>
+        public override string Description
+        {
+            get { return $"Logical name of {_scope} must match to regular expression pattern {_regexPattern}."; }
+        }
+
+        /// <summary>
+        /// Creates a new rule instance requiring naming to match given
+        /// pattern on certain scope.
+        /// </summary>
+        /// <param name="regexPattern">
+        /// Regular expression pattern which must be met on scope given by
+        /// parameter <paramref name="scope"/>.
+        /// </param>
+        /// <param name="scope">
+        /// Scope of the rule.
+        /// </param>
+        public RegexRule(string regexPattern, RuleScope scope)
+        {
+            try
+            {
+                _regexPattern = new Regex(regexPattern);
+            }
+            catch(ArgumentException ex) {
+                throw new ArgumentException(
+                    $"Given pattern \"{regexPattern}\" was invalid regular expression.",
+                    ex);
+            }
+
+            _scope = scope;
+        }
+
+        protected override ValidationResult ValidateRule(SolutionEntity solutionEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        private readonly Regex _regexPattern;
+        private readonly RuleScope _scope;
+
+    }
+}
---
+++ CdsCustomizationValidator/CdsCustomizationValidator.Test/Domain/Rule/RegexRuleTest.cs
@@ -0,0 +1,75 @@
+﻿using CdsCustomizationValidator.Domain;
+using CdsCustomizationValidator.Domain.Rule;
+using FakeXrmEasy.Extensions;
+using Microsoft.Xrm.Sdk.Metadata;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CdsCustomizationValidator.Test.Domain.Rule
+{
+
+    /// <summary>
+    /// Unit tests for rule <see cref="RegexRule"/>
+    /// </summary>
+    public class RegexRuleTests
+    {
+
+        [Fact(DisplayName = "RegexRule: Rule description must show pattern.")]
+        public void RuleDescriptionMustShowPatternTest()
+        {
+            var regexPattern = @"^[A-Za-z]+_[A-Z]{1}[a-z]{1}[A-Za-z]*$";
+            var scope = RuleScope.Entity;
+
+            var ruleToTest = new RegexRule(regexPattern, scope);
+
+            Assert.EndsWith($" must match to regular expression pattern {regexPattern}.",
+                            ruleToTest.Description);
+        }
+
+        [Fact(DisplayName = "RegexRule: Invalid pattern must throw an exception.")]
+        public void InvalidRegexPatternMustFailOnObjectInitialization()
+        {
+
+            var invalidPattern = "[";
+
+            var ex = Assert.Throws<ArgumentException>(
+                () => new RegexRule(invalidPattern, RuleScope.Entity));
+
+            Assert.Equal("Given pattern \"[\" was invalid regular expression.",
+                         ex.Message);
+            Assert.NotNull(ex.InnerException);
+            Assert.IsType<ArgumentException>(ex.InnerException);
+
+        }
+
+        [Fact(DisplayName = "RegexRule: Entity first letter is a capital letter.")]
+        public void EnsureEntityFirstLetterIsCapitalLetterTest() {
+            EntityMetadata entity = new EntityMetadata()
+            {
+                SchemaName = "foobar_MyMagnificientEntity",
+            };
+            entity.SetSealedPropertyValue("IsManaged", false);
+            entity.SetSealedPropertyValue("IsCustomEntity", true);
+
+            List<AttributeMetadata> attributes = null;
+
+            var isOwnedBySolution = true;
+
+            var validSolutionEntity = new SolutionEntity(entity,
+                                                         attributes,
+                                                         isOwnedBySolution);
+
+            var regexPattern = @"^[A-Za-z]+_[A-Z]{1}[a-z]{1}[A-Za-z]*$";
+            var scope = RuleScope.Entity;
+
+            var ruleToTest = new RegexRule(regexPattern, scope);
+
+            var results = ruleToTest.Validate(validSolutionEntity);
+
+            Assert.True(results.Passed);
+        }
+
+    }
+
+}
