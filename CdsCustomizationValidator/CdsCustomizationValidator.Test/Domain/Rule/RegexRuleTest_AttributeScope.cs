f398612549b159cdba605bae35e29e8895945bed
Teppo Lainio <teppo.lainio@gmail.com>
2020-01-03 15:11:47 +0200

    Regular expression validation rules for attributes.

--- CdsCustomizationValidator/CdsCustomizationValidator.Test/Domain/Rule/RegexRuleTest_AttributeScope.cs
+++ CdsCustomizationValidator/CdsCustomizationValidator.Test/Domain/Rule/RegexRuleTest_AttributeScope.cs
@@ -1,9 +1,8 @@
-﻿using CdsCustomizationValidator.Domain.Rule;
-using System;
+﻿using CdsCustomizationValidator.Domain;
+using CdsCustomizationValidator.Domain.Rule;
+using FakeXrmEasy.Extensions;
+using Microsoft.Xrm.Sdk.Metadata;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace CdsCustomizationValidator.Test.Domain.Rule
@@ -25,6 +24,222 @@ namespace CdsCustomizationValidator.Test.Domain.Rule
 
             Assert.Equal($"Schema name of an Attribute must match to regular expression pattern {regexPattern}.",
                          ruleToTest.Description);
+        }
+
+        [Fact(DisplayName = "RegexRule: Managed fields must be skipped on OOB entity.")]
+        public void ManagedOobAttributesMustBeSkippedTest()
+        {
+            EntityMetadata entity = new EntityMetadata()
+            {
+                SchemaName = "Account",
+            };
+            entity.SetSealedPropertyValue("IsManaged", true);
+            entity.SetSealedPropertyValue("IsCustomEntity", false);
+
+            var accountNameMetadata = new StringAttributeMetadata("Name");
+            accountNameMetadata.SetSealedPropertyValue("IsManaged", true);
+            accountNameMetadata.SetSealedPropertyValue("IsCustomAttribute", false);
+
+            var accountAddress1Line1Metadata = new StringAttributeMetadata("Address1_Line1");
+            accountAddress1Line1Metadata.SetSealedPropertyValue("IsManaged", true);
+            accountAddress1Line1Metadata.SetSealedPropertyValue("IsCustomAttribute", false);
+
+            var field1Metadata = new StringAttributeMetadata("foo_customField");
+            field1Metadata.SetSealedPropertyValue("IsManaged", true);
+            field1Metadata.SetSealedPropertyValue("IsCustomAttribute", true);
+
+            var attributes = new List<AttributeMetadata> {
+                accountNameMetadata,
+                accountAddress1Line1Metadata,
+                field1Metadata
+            };
+
+            var isOwnedBySolution = false;
+
+            var validSolutionEntity = new SolutionEntity(entity,
+                                                         attributes,
+                                                         isOwnedBySolution);
+
+            var regexPattern = @"^[A-Za-z]+_[A-Z]{1}[a-z]{1}[A-Za-z]*$";
+            var scope = RuleScope.Attribute;
+
+            var ruleToTest = new RegexRule(regexPattern, scope);
+
+            var results = ruleToTest.Validate(validSolutionEntity);
+
+            Assert.True(results.Passed);
+        }
+
+        [Fact(DisplayName = "RegexRule: Check musts succeedd for ustom unmanaged fields matching the pattern.")]
+        public void CustomAttributesMatchingPatternMustSucceedTest()
+        {
+            EntityMetadata entity = new EntityMetadata()
+            {
+                SchemaName = "Account",
+            };
+            entity.SetSealedPropertyValue("IsManaged", true);
+            entity.SetSealedPropertyValue("IsCustomEntity", false);
+
+            var field1Metadata = new StringAttributeMetadata("foo_CustomField");
+            field1Metadata.SetSealedPropertyValue("IsManaged", false);
+            field1Metadata.SetSealedPropertyValue("IsCustomAttribute", true);
+
+            var attributes = new List<AttributeMetadata> {
+                field1Metadata
+            };
+
+            var isOwnedBySolution = false;
+
+            var validSolutionEntity = new SolutionEntity(entity,
+                                                         attributes,
+                                                         isOwnedBySolution);
+
+            var regexPattern = @"^[A-Za-z]+_[A-Z]{1}[a-z]{1}[A-Za-z]*$";
+            var scope = RuleScope.Attribute;
+
+            var ruleToTest = new RegexRule(regexPattern, scope);
+
+            var results = ruleToTest.Validate(validSolutionEntity);
+
+            Assert.True(results.Passed);
+        }
+
+        [Fact(DisplayName = "RegexRule: Custom fields with incorrect names must fail on OOB entity.")]
+        public void UnmanagedAttributesOnOobEntityWithIncorrectNamesMustFailTest()
+        {
+            EntityMetadata entity = new EntityMetadata()
+            {
+                SchemaName = "Account",
+            };
+            entity.SetSealedPropertyValue("IsManaged", true);
+            entity.SetSealedPropertyValue("IsCustomEntity", false);
+
+            var field1Metadata = new StringAttributeMetadata("foo_customField");
+            field1Metadata.SetSealedPropertyValue("IsManaged", false);
+            field1Metadata.SetSealedPropertyValue("IsCustomAttribute", true);
+
+            var field2Metadata = new StringAttributeMetadata("foo_c");
+            field2Metadata.SetSealedPropertyValue("IsManaged", false);
+            field2Metadata.SetSealedPropertyValue("IsCustomAttribute", true);
+
+            var field3Metadata = new StringAttributeMetadata("foo_CustomField");
+            field3Metadata.SetSealedPropertyValue("IsManaged", false);
+            field3Metadata.SetSealedPropertyValue("IsCustomAttribute", true);
+
+            var attributes = new List<AttributeMetadata> {
+                field1Metadata,
+                field2Metadata,
+                field3Metadata
+            };
+
+            var isOwnedBySolution = false;
+
+            var validSolutionEntity = new SolutionEntity(entity,
+                                                         attributes,
+                                                         isOwnedBySolution);
+
+            var regexPattern = @"^[A-Za-z]+_[A-Z]{1}[a-z]{1}[A-Za-z]*$";
+            var scope = RuleScope.Attribute;
+
+            var ruleToTest = new RegexRule(regexPattern, scope);
+
+            var results = ruleToTest.Validate(validSolutionEntity);
+
+            Assert.False(results.Passed);
+        }
+
+        [Fact(DisplayName = "RegexRule: Custom fields with incorrect names failure description is correct.")]
+        public void UnmanagedAttributesOnOobEntityWithIncorrectNamesFailureDescriptionIsCorrectTest()
+        {
+            EntityMetadata entity = new EntityMetadata()
+            {
+                SchemaName = "Account",
+            };
+            entity.SetSealedPropertyValue("IsManaged", true);
+            entity.SetSealedPropertyValue("IsCustomEntity", false);
+
+            var field1Metadata = new StringAttributeMetadata("foo_customField");
+            field1Metadata.SetSealedPropertyValue("IsManaged", false);
+            field1Metadata.SetSealedPropertyValue("IsCustomAttribute", true);
+
+            var field2Metadata = new StringAttributeMetadata("foo_c");
+            field2Metadata.SetSealedPropertyValue("IsManaged", false);
+            field2Metadata.SetSealedPropertyValue("IsCustomAttribute", true);
+
+            var field3Metadata = new StringAttributeMetadata("foo_CustomField");
+            field3Metadata.SetSealedPropertyValue("IsManaged", false);
+            field3Metadata.SetSealedPropertyValue("IsCustomAttribute", true);
+
+            var attributes = new List<AttributeMetadata> {
+                field1Metadata,
+                field2Metadata,
+                field3Metadata
+            };
+
+            var isOwnedBySolution = false;
+
+            var validSolutionEntity = new SolutionEntity(entity,
+                                                         attributes,
+                                                         isOwnedBySolution);
+
+            var regexPattern = @"^[A-Za-z]+_[A-Z]{1}[a-z]{1}[A-Za-z]*$";
+            var scope = RuleScope.Attribute;
+
+            var ruleToTest = new RegexRule(regexPattern, scope);
+
+            var results = ruleToTest.Validate(validSolutionEntity);
+
+            Assert.Contains($"Following attributes do not match given pattern \"{regexPattern}\": foo_customField, foo_c.",
+                            results.FormatValidationResult());
+        }
+
+        [Fact(DisplayName = "RegexRule: Custom fields with incorrect names can be excluded from validation.")]
+        public void ExcludeUnmanagedAttributesWithIncorrectNamesFromValidationTest()
+        {
+            EntityMetadata entity = new EntityMetadata()
+            {
+                SchemaName = "Account",
+            };
+            entity.SetSealedPropertyValue("IsManaged", true);
+            entity.SetSealedPropertyValue("IsCustomEntity", false);
+
+            var field1Metadata = new StringAttributeMetadata("foo_customField");
+            field1Metadata.SetSealedPropertyValue("IsManaged", false);
+            field1Metadata.SetSealedPropertyValue("IsCustomAttribute", true);
+
+            var field2Metadata = new StringAttributeMetadata("foo_c");
+            field2Metadata.SetSealedPropertyValue("IsManaged", false);
+            field2Metadata.SetSealedPropertyValue("IsCustomAttribute", true);
+
+            var field3Metadata = new StringAttributeMetadata("foo_CustomField");
+            field3Metadata.SetSealedPropertyValue("IsManaged", false);
+            field3Metadata.SetSealedPropertyValue("IsCustomAttribute", true);
+
+            var attributes = new List<AttributeMetadata> {
+                field1Metadata,
+                field2Metadata,
+                field3Metadata
+            };
+
+            var isOwnedBySolution = false;
+
+            var validSolutionEntity = new SolutionEntity(entity,
+                                                         attributes,
+                                                         isOwnedBySolution);
+
+            var regexPattern = @"^[A-Za-z]+_[A-Z]{1}[a-z]{1}[A-Za-z]*$";
+            var scope = RuleScope.Attribute;
+            var excludedAttributes = new List<string> {
+                "foo_customField",
+                "foo_c"
+            };
+
+            var ruleToTest = new RegexRule(regexPattern, scope,
+                                           excludedAttributes);
+
+            var results = ruleToTest.Validate(validSolutionEntity);
+
+            Assert.False(results.Passed);
         }
 
     }
